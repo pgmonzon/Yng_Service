b4658604edbc9165e632f33fa6e22909aaddc8c0
Adrian Dias da Costa Lima <adrian.diasdacostalima@gmail.com>
2016-10-13 11:48:39 -0300

    rbac

---
+++ handlers/rbac.go
@@ -0,0 +1,101 @@
+package handlers
+
+// TODO: Mucho copypaste, se tiene que poder simplificar.
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/pgmonzon/Yng_Servicios/models"
+	"github.com/pgmonzon/Yng_Servicios/core"
+
+	//"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func ListarRoles(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	var roles []models.Roles
+	session := core.Session.Copy()
+	defer session.Close()
+	collection := session.DB(core.Dbname).C("roles")
+	collection.Find(bson.M{}).All(&roles)
+	response, err := json.MarshalIndent(roles, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	core.JSONResponse(w, r, start, response, http.StatusOK)
+}
+
+// TodoAdd handler to add new todo
+func AgregarRol(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	var rol models.Roles
+	json.NewDecoder(r.Body).Decode(&rol)
+	if rol.Nombre == "" {
+		core.JSONError(w, r, start, "Incorrect body", http.StatusBadRequest)
+		return
+	}
+	objID := bson.NewObjectId()
+	rol.ID = objID
+	session := core.Session.Copy()
+	defer session.Close()
+	collection := session.DB(core.Dbname).C("roles")
+	err := collection.Insert(rol)
+	if err != nil {
+		core.JSONError(w, r, start, "Failed to insert rol", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Location", r.URL.Path+"/"+string(rol.ID.Hex()))
+	core.JSONResponse(w, r, start, []byte{}, http.StatusCreated)
+}
+
+
+
+
+
+
+
+
+
+//
+//------  PERMISOS  ------
+//
+
+func ListarPermisos(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	var permisos []models.Permisos
+	session := core.Session.Copy()
+	defer session.Close()
+	collection := session.DB(core.Dbname).C("permisos")
+	collection.Find(bson.M{}).All(&permisos)
+	response, err := json.MarshalIndent(permisos, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	core.JSONResponse(w, r, start, response, http.StatusOK)
+}
+
+// TodoAdd handler to add new todo
+func AgregarPermiso(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	var permiso models.Permisos
+	json.NewDecoder(r.Body).Decode(&permiso)
+	if permiso.Nombre == "" {
+		core.JSONError(w, r, start, "Incorrect body", http.StatusBadRequest)
+		return
+	}
+	objID := bson.NewObjectId()
+	permiso.ID = objID
+	session := core.Session.Copy()
+	defer session.Close()
+	collection := session.DB(core.Dbname).C("permisos")
+	err := collection.Insert(permiso)
+	if err != nil {
+		core.JSONError(w, r, start, "Failed to insert permiso", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Location", r.URL.Path+"/"+string(permiso.ID.Hex()))
+	core.JSONResponse(w, r, start, []byte{}, http.StatusCreated)
+}
---
+++ models/rbac.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+//Todo struct to todo
+type Roles struct {
+	ID        bson.ObjectId `bson:"_id" json:"id"`
+	Nombre    string        `json:"rol"`
+	Activo    bool          `json:"activo"`
+	Borrado   bool          `json:"borrado"`
+}
+
+//Todo struct to todo
+type Permisos struct {
+	ID        bson.ObjectId `bson:"_id" json:"id"`
+	Nombre    string        `json:"permiso"`
+	Activo    bool          `json:"activo"`
+	Borrado   bool          `json:"borrado"`
+}
+
+//Todo struct to todo
+type RP struct {
+	ID             bson.ObjectId `bson:"_id" json:"id"`
+	IDRol          string        `json:"rol"`
+	IDPermiso      string        `json:"permiso"`
+}
